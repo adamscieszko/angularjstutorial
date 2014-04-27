1f880821077aa46b03301c7f7701a154270bf799
Adam Ścieszko <adam.scieszko@gmail.com>
2014-04-27 21:33:52 +0200

    Add step2 and step3

--- HelloAngular/app.js
+++ HelloAngular/app.js
@@ -1,9 +1,29 @@
 var app = angular.module("myApp", []);
 
-app.controller('FirstCtrl', function ($scope) {
+app.controller("FirstCtrl", function ($scope) {
 	$scope.model = { firstName: "Gabriel", lastName: "Schenker" };
 
 	$scope.clickMe = function(name){
 		alert("Hello " + name);
+	};
+});
+
+app.controller("ParentCtrl", function($scope){
+	$scope.model = { title: "Title set by parent" };
+
+	$scope.greet = function(){
+		alert("Hi, I'm the parent");
+	};
+});
+
+app.controller("ChildCtrl", function($scope){
+	$scope.content = "Content set by child";
+
+	$scope.setModel = function(){
+		$scope.model = { title: "Test from child" };
+	};
+
+	$scope.greet = function(){
+		alert("Hi, I'm the child");
 	};
 });
---
+++ HelloAngular/step4.js
@@ -0,0 +1,11 @@
+var app = angular.module("step4App", []);
+
+app.controller("FriendsCtrl", function($scope, $http){
+	$scope.loadFriends = function(){
+		$http.get("api/friends").success(function(data){
+			$scope.friends = data;
+		}).error(function(){
+			alert("an unexpected error ocurred!");
+		});
+	};
+});
